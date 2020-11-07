661c9fd8aede4bcac2f1fa419c40fcfb4f1560c1
khyejin1231 <74085996+khyejin1231@users.noreply.github.com>
2020-11-07 19:45:48 +0900

    Add files via upload

--- Assinment_2.R
+++ Assinment_2.R
@@ -10,7 +10,7 @@ elastic_net <- function (X , y , eps , lambda , alpha ) {
   #initialization 
   n <- nrow(X)
   beta_k1 <- rep(0, ncol(X))
-  XtX <- crossprod(X,X); Xty <- crossprod(X,y); yty <- crossprod(y,y)
+  XtX <- crossprod(X,X); Xty <- t(X) %*% y; yty <- crossprod(y,y)
   k <- 1 ; L_diff <- 1; L_beta_k <- 2; L_beta_k1 <- 1
   
   D <- function (beta) diag(1 / max(abs(beta) , eps), ncol(X), ncol(X)) #can I use the pre-defined alpha and lambda here?
@@ -27,7 +27,7 @@ elastic_net <- function (X , y , eps , lambda , alpha ) {
     c_new <- c(beta_k)
     A <- XtX/n + diag(lambda * (1 - alpha), ncol(X), ncol(X)) + lambda * alpha * D_new
     
-    beta_k1 <- solve (A, Xty/n)
+    beta_k1 <- solve (A, Xty/n) #
     
     L_beta_k1 <- (0.5)* (t(beta_k1) %*% ((A) %*% beta_k1)) - (t(beta_k1) %*% Xty)/n + c_new
 
@@ -40,12 +40,11 @@ elastic_net <- function (X , y , eps , lambda , alpha ) {
 
 #K-fold cross-validation
 k_cv <- function (X , y, k_fold , eps , lambdas , alpha ){
-  X <- data.matrix(X); y <- data.matrix(y)
   eps <- eps; alpha <- alpha
   n <- nrow(X)
   m <- n/(k_fold) #But what if n/k is not a whole number?
   RMSE <- matrix(NA, nrow = length(lambdas), ncol = 2)
-  MSE <- matrix(NA, nrow = length(k_fold), ncol = 1)
+  MSE <- matrix(NA, nrow = k_fold, ncol = 1)
   for (i in 1:length(lambdas)){ #???
     lambda <- lambdas[i]
     RMSE[i,1] <- lambdas[i]
@@ -55,8 +54,8 @@ k_cv <- function (X , y, k_fold , eps , lambdas , alpha ){
         X_test <- X[ (1 : m) , ] ;  y_test <- y[ (1 : m) , ]
         
       } else{ 
-        X_train <- X[ -((k-1) * m : k * m ), ]; y_train <- y[-((k-1) * m : k * m ), ]
-        X_test <- X[ ((k-1) * m : k * m ), ]; y_test <- y[((k-1) * m : k * m ), ]
+        X_train <- X[-((k - 1) * m : (k * m) ), ]; y_train <- y[-((k - 1) * m : k * m ), ]
+        X_test <- X[ (k - 1) * m : (k * m) , ]; y_test <- y[(k - 1) * m : (k * m) , ]
       }
       beta_k1 <- elastic_net(X_train , y_train , eps , lambda , alpha )
       y_hat <- X_test %*% beta_k1
@@ -68,20 +67,23 @@ k_cv <- function (X , y, k_fold , eps , lambdas , alpha ){
   }
 
 }
-
+dim(X)
+dim(y2)
+as.matrix(y)
+y2 <- as.matrix(y)
 #Main
 load("supermarket1996.RData")
 data<-supermarket1996
 colnames(data)
 data<-data[,-which(colnames(data) == c("STORE", "CITY", "ZIP", "GROCCOUP_sum", "SHPINDX"))]
 colnames(data[,3:46]) 
-X <- data[,3:46]
-y <- data$GROCERY_sum
+X <- as.matrix(data[,3:46])
+y <- as.matrix(data$GROCERY_sum)
 results <- k_cv (X , y, 11 , 1e-8 , (0.001:100) , 0.5 )
-
+X[ (( 11- 1) * 7 : 11 * 7 ), ]
 #Purchase behavior & availabbility of time
 #Purchase behavior and income
   
-  
-  
-  
+Xty <- crossprod(X,y)
+X  
+y  
